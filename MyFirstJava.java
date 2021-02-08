9ed362bd31fbe38555855ee7671ed906de8ce942
Brendan McCullagh <47070839+Static-Line@users.noreply.github.com>
2021-02-07 21:55:50 -0600

    Add files via upload

---
+++ MyFirstJava.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+class MyFirstJava
+{
+public static void main(String args[])
+{
+System.out.println("Welcome to ENTD380, Introduction to Object Oriented Programming with Java");
+System.out.println("Have a great learning experience");
+
+}
+}
